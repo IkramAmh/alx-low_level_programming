4f1e5bfb8ee485cc3edce71507e45d2ddce8843d
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-21 13:29:18 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
- * Return : always 0.
+ * return : always 0.
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
