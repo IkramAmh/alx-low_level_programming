d2a4073770199052eec67e7ad13f686c9f66fc5e
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-21 14:19:15 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int c, int i, int li, int lli, int f)
+{
+	c=sizeof(char);
+	i=sizeof(int);
+	li=sizeof(long int);
+	lli=sizeof(long long int);
+	f=sizeof(float);
+
+	printf("Size of a char: "&c" byte\(s\)");
+	printf("Size of an int: "&i" byte\(s\)");
+	printf("Size of a long int: "&li" byte\(s\)");
+	printf("Size of a long long int: "&lli" byte\(s\)");
+	printf("Size of a float: "&f" byte\(s\)");
+	return (0);
+}
