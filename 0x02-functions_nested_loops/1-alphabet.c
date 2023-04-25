38fa083fde3a65aab0313463963a4a6a66af92cf
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-25 11:39:54 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,12 @@
  * Return : always 0.
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char c;
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)	
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
