66c1e009ed2ea699c034d174e29cff08eada635d
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-25 11:20:08 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**main - prints alphabets
+ * in lowercase
+ * Return : always 0.
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
