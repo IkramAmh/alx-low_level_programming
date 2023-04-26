91479fbb39b0267ff873490bead21b56eab2f018
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-25 18:55:40 -0700

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,23 +12,23 @@ void jack_bauer(void)
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
 			{
-				for (c = 0; c <= 5; c++)
+				for (d = 0; d <= 9; d++)
 				{
-					for (d = 0; d <= 9; d++)
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(58);
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+					if (a == 2 && b == 3 && c == 5 && d == 9)
 					{
-						_putchar(a + '0');
-						_putchar(b + '0');
-						_putchar(58);
-						_putchar(c + '0');
-						_putchar(d + '0');
-						_putchar('\n');
-						if (a == 2 && b == 3 && c == 5 && d == 9)
-						{
-							return;
-						}
+						return;
 					}
 				}
 			}
+		}
 	}
 }
