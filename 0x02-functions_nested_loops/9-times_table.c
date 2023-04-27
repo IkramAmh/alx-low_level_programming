364106e9697135f1ef90e785b668919803105146
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-26 17:19:57 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 9; j++)
 		{
 			x = i * j;
 			if (x <= 9)
@@ -25,6 +25,19 @@ void times_table(void)
 			_putchar((x % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
+		}
+		if (j == 9)
+		{
+			x = i * j;
+			if (x <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
 	}
