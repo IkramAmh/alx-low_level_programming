d368b871f947edb0813115054172247c1b3a974a
IkramAmh <ikramamhirrach2@gmail.com>
2023-04-28 18:41:39 -0700

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - returns the case of a character
+ * @c: The character to be checked
+ * Return: 1 if uppercase 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
