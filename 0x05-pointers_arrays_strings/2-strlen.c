30ee41b865ca15e9cf86c263dbfe86ab591b2c62
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-02 19:17:38 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The string to be measured
+ * Return: The length of s
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	
+	while (*s++)
+	{
+		length++;
+	}
+	return (length);
+}	
