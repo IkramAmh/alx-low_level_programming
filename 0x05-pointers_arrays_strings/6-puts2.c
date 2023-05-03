7e1c67bbbf0cdc18a36a053fa047c3a5b22e9227
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-03 07:19:56 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,16 @@
 
 void puts2(char *str)
 {
-	int i, len = 0;
+	int i;
+	int len = 0;
 
-	while (str++)
+	while (str != '\0')
 	{
 		len++;
 	}
-	for (i = 0; i <= len - 1; i++)
+	for (i = 0; i <= len; i += 2)
 	{
-		_putchar(str[0]);
-		str[i] = *(str + 2);
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
