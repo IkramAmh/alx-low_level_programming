27e869ed7df96bdb642be4de20888ab30554c5e2
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-03 18:21:01 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the seconf half of a string
+ * @str: The string to be printed
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if ((len - 1) % 2 != 0)
+	{
+		for (i = ((len - 1) / 2) + 1; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (len - 1) / 2; i <= len - 1; i++)
+		{
+			_putchar(str[i]);
+		}
+	}	
+	_putchar('\n');
+}
