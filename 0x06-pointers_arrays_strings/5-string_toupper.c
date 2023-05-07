257299b5897da030fc71a9f9a5ccb8df9928679e
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-07 16:13:59 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts uppercases to lowercases
+ * @a: The array to be converted
+ * Return: Returns a in lowercase.
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{	
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] -= 32;
+			i++;
+		}
+	}
+	return (a);
+}
