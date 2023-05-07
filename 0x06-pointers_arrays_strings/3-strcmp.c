6b567f4a895c635d50fd4a85698acde1dd940c3e
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-07 11:01:08 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int n;
-
-	n = s1[i] - s2[i];
-	while (*s1 && *s2 && n == 0)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
 		s1++;
 		s2++;
-		n = s1++ - s2++;
 	}
-	return (n);
+	return (*s1 - *s2);
 }
