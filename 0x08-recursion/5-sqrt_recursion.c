8158e1c791fea4e4932d9a9c775bd17ddea6029a
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-16 07:18:46 -0700

    5-sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * sqrt - checks the input number from i to n
+ * @i: The starter
+ * @n: The base
+ * Return: The value of i.
+ */
+
+int _sqrt(int i, int n)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	return (_sqrt(i + 1, n));
+}
+/**
+ * _sqrt_recursion - prints the square root of n
+ * @n: The number to calculate its sqrt
+ * Return: The square root of n.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(1, n));
+}
