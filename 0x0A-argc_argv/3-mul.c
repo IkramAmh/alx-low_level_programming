8dbd502680b44c9d500cb6d65136be1d6c159942
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-29 07:11:46 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,11 +10,14 @@
 
 int main(int argc, char *argv[])
 {
+	int prod;
+
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	prod = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", prod);
 	return (0);
 }
