2959107c38dc29202b69c3b4a75a18aac8ebaf67
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-29 04:28:26 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -8,13 +9,13 @@
  * Return: always successful.
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", (int) *argv[1] * (int) *argv[2]);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
