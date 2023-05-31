1fed19af808b762f7dba3955ac51b579a1c1b518
IkramAmh <ikramamhirrach2@gmail.com>
2023-05-31 02:49:08 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints te addition of the elements inputed
+ * to the program
+ * @argc: the number of arguments
+ * @argv: the pointer to the arguments
+ * Return: 0 is success 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, tot = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] < 48 || *argv[i] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			else
+			{
+				tot = tot + atoi(argv[i]);
+			}
+		}
+	printf("%d\n", tot);
+	}
+	return (0);
+}
