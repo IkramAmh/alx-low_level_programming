6d9f9f7c30583c459a166abcf543f365e51d475f
IkramAmh <ikramamhirrach2@gmail.com>
2023-07-03 03:55:35 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,14 +6,13 @@
  * main - calculate the product of two integers
  * argc: The number of arguments
  * argv: arguments passed into the function
- * Return: 0 if success 1 otherwise
+ * Return: 0 if success 1 otherwise.
  */
-
 int main(int argc, char *argv[])
 {
 	int prod;
 
-	if (argc > 1)
+	if (argc > 1 && argc <= 3)
 	{
 		prod = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", prod);
