b9555843dc222302ab88b0b58c213d56dc00ee5f
IkramAmh <ikrmamhirrach2@gmail.com>
2023-09-28 17:52:34 -0700

    100-palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+int palength(char *s);
+int strcomp(char *s, int i, int l);
+
+/**
+ * is_palindrome - returns if s is a palindrome
+ * @s: the string to be checked
+ * Return: 1 if true 0 otherwise.
+ */
+
+int is_palindrome(char *s)
+{
+        int l;
+
+        l = palength(s);
+        return (strcomp(s, 0, l));
+}
+
+/**
+ * palength - measures the length of the string
+ * @s: the string to be measured
+ * Return: the length of the string.
+ */
+
+int palength(char *s)
+{
+	if (*s == 0)
+	{
+		return (0);
+	}
+	return (1 + palength(s + 1));
+}
+
+/**
+ * strcomp - compares the characters of the string
+ * @s: the string to check
+ * @l: the string's length
+ * Return: 1 if true 0 otherwise.
+ */
+
+int strcomp(char *s, int i, int l)
+{
+	if (*(s + i) != *(s + (l - 1)))
+	{
+		return (0);
+	}
+	return (1);
+	return (strcomp(s + 1, i + 1, l - 1));
+}
