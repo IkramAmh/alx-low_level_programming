03ec7dff14f6a61d83095db64ad87ef9c24c354b
IkramAmh <ikramamhirrach2@gmail.com>
2023-12-18 15:48:34 -0800

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,5 +37,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(fp, text_content, len);
 	close(fp);
-	return(bytes == len ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
