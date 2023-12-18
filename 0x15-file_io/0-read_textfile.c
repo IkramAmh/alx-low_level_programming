a540ec52c63827f7214ee8d3c9f72ccc63772062
IkramAmh <ikramamhirrach2@gmail.com>
2023-12-18 14:56:18 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename || ! letters)
+	if (!filename || !letters)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
 	bytes = read(fp, &buf[0], letters);
-	bytes = write(fp, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], letters);
 	close(fp);
 	return (bytes);
 }
