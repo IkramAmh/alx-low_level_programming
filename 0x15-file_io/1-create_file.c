e6b48365c199c64d69da9a74bf129f570d9fb41c
IkramAmh <ikramamhirrach2@gmail.com>
2024-02-19 01:32:05 -0800

    b

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,6 +34,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		wr = write(fd, "", 0);
 	if (len)
 		wr = write(fd, text_content, len);
 	close(fd);
