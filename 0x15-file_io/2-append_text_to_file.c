a31846a0469b0fc75419e22b0c020d917ca7adad
IkramAmh <ikramamhirrach2@gmail.com>
2024-02-22 01:22:41 -0800

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -32,12 +32,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd = -1)
+	if (fd == -1)
 		return (-1);
 	while (len)
 		wr = write(fd, text_content, len);
 	if (len == 0)
-		close(fd);
+	{
+	}
 	close(fd);
 	return (wr == len ? 1 : -1);
 }
