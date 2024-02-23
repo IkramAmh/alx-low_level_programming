e52dfc84133860cda39de213ff852ea322e90617
IkramAmh <ikramamhirrach2@gmail.com>
2024-02-23 01:55:25 -0800

    c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
 /**
  * _strlen -  measures the length of a string
  * @s: the string to measure
@@ -33,7 +31,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND, PERMISSIONS);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	while (len)
